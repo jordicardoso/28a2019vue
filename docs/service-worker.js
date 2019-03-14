a48414c85d44c038a8ea896b246578f7e2e82367
Jordi Cardoso <jordi.cardoso@bonpreu.cat>
2019-03-14 17:31:05 +0100

    test

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/28a2019vue/precache-manifest.dd7ffaa349008fb3410c6c1875c89dc4.js"
+  "/28a2019vue/precache-manifest.cd74684ff3f372db93edef477db4db47.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "28a2019vue"});

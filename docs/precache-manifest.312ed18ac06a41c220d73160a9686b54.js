5d3789aa89e22acb696368f67c6294b8d3b06622
Jordi Cardoso <jordi.cardoso@bonpreu.cat>
2019-03-26 11:58:32 +0100

    test

---
+++ docs/precache-manifest.312ed18ac06a41c220d73160a9686b54.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/28a2019vue/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "5eb9fa9133e54e0d594d",
+    "url": "/28a2019vue/js/app.a2e470ad.js"
+  },
+  {
+    "revision": "c6ff57aba64444a764dc",
+    "url": "/28a2019vue/js/chunk-2d0c0fc5.d0071b12.js"
+  },
+  {
+    "revision": "04a2f240ef4bb170163b",
+    "url": "/28a2019vue/js/chunk-2d0d9f7e.b5f6ee03.js"
+  },
+  {
+    "revision": "cfae489c846cae3e2d54",
+    "url": "/28a2019vue/js/chunk-2d0e663b.c15fc750.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/28a2019vue/robots.txt"
+  },
+  {
+    "revision": "8086b94d4caef4bf7199",
+    "url": "/28a2019vue/js/chunk-3a130f61.961e27c2.js"
+  },
+  {
+    "revision": "601fa92a56cc1cfff200",
+    "url": "/28a2019vue/js/chunk-2d0ae8a8.efe8e7b7.js"
+  },
+  {
+    "revision": "9e2f1ba6517577946d8e",
+    "url": "/28a2019vue/js/chunk-52a36338.86f8276a.js"
+  },
+  {
+    "revision": "6ecc3e85212ccd546c25",
+    "url": "/28a2019vue/js/chunk-vendors.087f4f43.js"
+  },
+  {
+    "revision": "ea1d2606f5f1ef24118c",
+    "url": "/28a2019vue/js/chunk-c1603f90.126d5ca5.js"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/28a2019vue/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "6c97061e332f9b62bdeafbb2bc253289",
+    "url": "/28a2019vue/data/vots_per_mesa.json"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/28a2019vue/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/28a2019vue/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/28a2019vue/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/28a2019vue/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/28a2019vue/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/28a2019vue/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/28a2019vue/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/28a2019vue/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/28a2019vue/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/28a2019vue/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/28a2019vue/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/28a2019vue/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "dc548badd4a2782db78e6d5c590898bb",
+    "url": "/28a2019vue/data/vots_per_municipi.json"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/28a2019vue/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/28a2019vue/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/28a2019vue/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/28a2019vue/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/28a2019vue/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d15084643b045f63e1c6c8725d09e235",
+    "url": "/28a2019vue/index.html"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/28a2019vue/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/28a2019vue/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/28a2019vue/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/28a2019vue/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/28a2019vue/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/28a2019vue/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/28a2019vue/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/28a2019vue/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/28a2019vue/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "678475278369f677002f93333cd348a4",
+    "url": "/28a2019vue/data/meses_pendents.json"
+  },
+  {
+    "revision": "ee1775586a5002d966e4eb4814769f13",
+    "url": "/28a2019vue/data/esconsTarragona.json"
+  },
+  {
+    "revision": "3d9d5d7167518b6aa9f67eeec910e322",
+    "url": "/28a2019vue/data/esconsLleida.json"
+  },
+  {
+    "revision": "e1b4f12c53128d15bf4f0a19cb26b6f9",
+    "url": "/28a2019vue/data/esconsGirona.json"
+  },
+  {
+    "revision": "3f6549fe00952e593726f66a6373b157",
+    "url": "/28a2019vue/data/esconsBarcelona.json"
+  },
+  {
+    "revision": "aaf95e67f62976c4c13749ba851c2668",
+    "url": "/28a2019vue/data/escons.json"
+  },
+  {
+    "revision": "6ecc3e85212ccd546c25",
+    "url": "/28a2019vue/css/chunk-vendors.8a7aacc4.css"
+  },
+  {
+    "revision": "ea1d2606f5f1ef24118c",
+    "url": "/28a2019vue/css/chunk-c1603f90.d61ca786.css"
+  },
+  {
+    "revision": "9e2f1ba6517577946d8e",
+    "url": "/28a2019vue/css/chunk-52a36338.0c58c949.css"
+  },
+  {
+    "revision": "8086b94d4caef4bf7199",
+    "url": "/28a2019vue/css/chunk-3a130f61.f1877e01.css"
+  }
+];
